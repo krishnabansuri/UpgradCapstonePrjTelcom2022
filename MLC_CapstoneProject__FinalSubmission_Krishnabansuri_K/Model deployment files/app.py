08ae1ab928d988b2977c0cff31ebbaac8585d7e8
krishnabansuri <krishnabansuri@gmail.com>
2023-02-01 23:36:47 +0530

    part 2 submission

--- MLC_CapstoneProject__FinalSubmission_Krishnabansuri_K/Model deployment files/app.py
+++ MLC_CapstoneProject__FinalSubmission_Krishnabansuri_K/Model deployment files/app.py
@@ -2,12 +2,12 @@ import numpy as np
 import pandas as pd
 from flask import Flask, request, jsonify, render_template
 import pickle
+from scipy.sparse import load_npz
+
 
 app = Flask(__name__)
-#gender_model = pickle.load(
-    #open('gender_model.pkl', 'rb'))
-#age_model = pickle.load(
-    #open('age_model.pkl', 'rb'))
+gender_model = pickle.load(open('/app/gender_model.pkl', 'rb'))
+age_model = pickle.load(open('/app/age_model.pkl', 'rb'))
 
 @app.route('/')
 def home():
@@ -19,19 +19,15 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-    #gender_test_data=pd.read_csv('../X_test_gender.npz.csv') 
-    #gender_prediction = gender_model.predict(gender_test_data.head(50))
-    #gender_output = round(gender_prediction[0], 2)
-    gender_output = 1
+    gender_test_data= load_npz('/app/X_test_gender.npz') 
+    gender_prediction = gender_model.predict(gender_test_data[50:])
+    gender_output = round(gender_prediction[0], 2)
     
-    #age_test_data=pd.read_csv('../X_test_age.npz.csv') 
-    #age_prediction = age_model.predict(age_test_data.head(50))
-    #age_output = round(age_prediction[0], 2)
-    age_output = 2
-
-    #return render_template('index.html', prediction_text='Gender and Age predictions for 50 Device ids {},{}'.format(gender_output,age_output))
-    
-    return render_template('index.html', prediction_text='Gender and Age predictions for 50 Device ids {}'.format(gender_output))
+    age_test_data=load_npz('/app/X_test_age.npz') 
+    age_prediction = age_model.predict(age_test_data[50:])
+    age_output = round(age_prediction[0], 2)
+ 
+    return render_template('index.html', prediction_text='Gender and Age predictions for 50 Device ids {},{}'.format(gender_output,age_output))
 
 
 if __name__ == "__main__":
